80edb4c8d0dfffc992f809afa02fcf8f90441371
pgod <p.godziszewski@o2.pl>
2016-08-08 14:40:29 +0200

    Multipart config & exception controller class
    Migreted multipart config to newer lib, separated exceptions from
    controllers to single class

--- src/main/java/godziszewski/patryk/ElectronicsStore/config/ElectronicsStoreWebInitialiser.java
+++ src/main/java/godziszewski/patryk/ElectronicsStore/config/ElectronicsStoreWebInitialiser.java
@@ -1,5 +1,12 @@
 package godziszewski.patryk.ElectronicsStore.config;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 
@@ -19,5 +26,26 @@ public class ElectronicsStoreWebInitialiser extends AbstractAnnotationConfigDisp
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
+
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(
+				new MultipartConfigElement("/tmp/images",
+						2097152, 4194304, 0));
+	}
+
+	
+	//Spring security
+	/*@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.setInitParameter("contextConfigLocation", "/WEB-INF/security-context.xml");
+		servletContext.addListener(ContextLoaderListener.class);
+		servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy("springSecurityFilterChain"))
+        .addMappingForUrlPatterns(null, false, "/*");
+		servletContext.setInitParameter("contextConfigLocation", "/WEB-INF/security-context.xml");
+		
+		
+	}*/
+	
 
 }
