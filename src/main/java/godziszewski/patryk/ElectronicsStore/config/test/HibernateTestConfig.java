d44a7f60013202a1215aad459fc93de4c5255719
pgod <p.godziszewski@o2.pl>
2016-09-07 20:37:28 +0200

    Test database, removed useless unit tests
    Added alternate database config for hibernate, using hsql database - db
    stored in memory. Added some integration tests for Customer Service.

---
+++ src/main/java/godziszewski/patryk/ElectronicsStore/config/test/HibernateTestConfig.java
@@ -0,0 +1,49 @@
+package godziszewski.patryk.ElectronicsStore.config.test;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+
+@Profile("InMemoryDatabaseDatabase")
+@Configuration
+public class HibernateTestConfig {
+	@Autowired
+	Environment environment;
+	
+	@Bean
+	@Primary
+	public LocalSessionFactoryBean sessionFactory() {
+       LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+       factoryBean.setDataSource(dataSource());
+       factoryBean.setPackagesToScan("godziszewski.patryk.ElectronicsStore.domain");
+       factoryBean.setHibernateProperties(hibernateProperties());
+       return factoryBean;
+       }
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		DriverManagerDataSource dm = new DriverManagerDataSource();
+		dm.setDriverClassName("org.hsqldb.jdbcDriver");
+		dm.setUrl("jdbc:hsqldb:mem:ElectronicsStoreTestDatabase");
+		dm.setUsername("sa");
+		dm.setPassword("");
+		return dm;
+		}
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+	    properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+	    properties.put("hibernate.show_sql", "true");
+	    properties.put("hibernate.hbm2ddl.auto", "create");
+	    return properties;
+	    } 
+}
---
+++ src/test/java/godziszewski/patryk/ElectronicsStore/service/CustomerServiceIntegrationTest.java
@@ -0,0 +1,150 @@
+package godziszewski.patryk.ElectronicsStore.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import godziszewski.patryk.ElectronicsStore.config.RootConfig;
+import godziszewski.patryk.ElectronicsStore.domain.Customer;
+import godziszewski.patryk.ElectronicsStore.domain.Order;
+
+
+import static org.junit.Assert.*;
+@ActiveProfiles("InMemoryDatabaseDatabase")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = RootConfig.class)
+public class CustomerServiceIntegrationTest {
+	@Autowired
+	CustomerService customerService;
+	@Autowired
+	OrderService orderService;
+	
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void addCorrectCustomerTest()
+	{
+		Customer newCustomer = this.createCustomer();
+		customerService.create(newCustomer);
+		
+		Customer readCustomer = customerService.getCustomerByEmail(newCustomer.getEmail());
+		
+		Assert.notNull(readCustomer);
+		assertEquals(newCustomer, readCustomer);
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void addIncorrectCustomerTest()
+	{
+		Exception caughtException = null;
+		Customer newCustomer = new Customer();
+		newCustomer.setName("");
+		newCustomer.setEmail("");
+		newCustomer.setPassword("");
+		
+		try{
+			customerService.create(newCustomer);
+		}catch (Exception ex)
+		{
+			caughtException = ex;
+		}
+		
+		Assert.notNull(caughtException);
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void getIncorrectCustomerByEmailTest()
+	{
+		Customer foundCustomer = customerService.getCustomerByEmail("");
+		
+		assertNull(foundCustomer);
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void updateCorrectCustomerTest()
+	{
+		Customer oldCustomer = this.createCustomer();
+		customerService.create(oldCustomer);
+		Customer newCustomer = new Customer();
+		newCustomer.setEmail(oldCustomer.getEmail());
+		newCustomer.setName("Micha³");
+		newCustomer.setSurname("Bia³ek");
+		newCustomer.setAreaName("Warszawa");
+		newCustomer.setState("Wykopowa");
+		newCustomer.setDoorNo("19");
+		newCustomer.setPassword("haslo");
+		newCustomer.setZipCode("11-111");
+		newCustomer.setPhoneNumber("100200300");
+		
+		customerService.update(newCustomer);
+		Customer foundCustomer = customerService.getCustomerByEmail(newCustomer.getEmail());
+		
+		assertEquals(newCustomer, foundCustomer);
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void updateIncorrectCustomerTest()
+	{
+		Customer customer = new Customer();
+		Exception caughtException = null;
+		try{
+			customerService.update(customer);
+		} catch(Exception ex)
+		{
+			caughtException = ex;
+		}
+		Assert.notNull(caughtException);
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void getCorrectCustomerByIdTest()
+	{
+		Customer newCustomer = this.createCustomer();
+		customerService.create(newCustomer);
+		
+		Customer foundCustomer = customerService.getCustomerById(newCustomer.getCustomerId());
+
+		assertEquals(newCustomer, foundCustomer);
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void getIncorrectCustomerByIdTest()
+	{	
+		Customer foundCustomer = customerService.getCustomerById(-1);
+
+		assertNull(foundCustomer);
+	}
+	private Customer createCustomer()
+	{
+		Customer customer = new Customer();
+		customer.setName("Jan");
+		customer.setSurname("Kowalski");
+		customer.setAreaName("Chrzanów");
+		customer.setStreetName("Przyk³adowa");
+		customer.setCountry("Poland");
+		customer.setDoorNo("1");
+		customer.setPassword("testPassword");
+		customer.setEmail("testEmail@gmail.com");
+		customer.setPhoneNumber("123456789");
+		customer.setState("ma³opolskie");
+		customer.setZipCode("22-100");
+		Set<Order> orders = new HashSet<Order>();		
+		customer.setOrders(orders);		
+		return customer;
+	}
+}
